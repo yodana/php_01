4c1613ebfa4438ece04fcb37d446dffbd6f4784f
DANA <dana.yohann@gmail.com>
2022-07-01 01:46:17 -0400

    ex00 done

---
+++ ex00/var.php
@@ -0,0 +1,10 @@
+<?php
+$a = 10;
+$b = "10";
+$c = "ten";
+$d = 10.0;
+
+echo "a contains : " . $a . " and has type : " . gettype($a) . PHP_EOL;
+echo "b contains : " . $b . " and has type : " . gettype($b) . PHP_EOL; 
+echo "c contains : " . $c . " and has type : " . gettype($c) . PHP_EOL; 
+echo "d contains : " . $d . " and has type : " . gettype($d) . PHP_EOL; 
