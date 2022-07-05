0bec7aa4391c1c6bc68d0d79a4cf37340a6d7fce
DANA <dana.yohann@gmail.com>
2022-07-04 23:17:58 -0400

    correc ex02

--- ex02/array2hash.php
+++ ex02/array2hash.php
@@ -1,8 +1,11 @@
 <?php
 function array2hash($array){
     $new = [];
+    if (gettype($array) != "array")
+        return ;
     foreach ($array as $a){
         $new[$a[1]] = $a[0];
     }
     return $new;
 }
+
