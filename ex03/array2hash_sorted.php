102d8e208512f46f0a9138f53b4a7aeff9cd11dd
DANA <dana.yohann@gmail.com>
2022-07-05 22:36:04 -0400

    ex03 en cours

--- ex03/array2hash_sorted.php
+++ ex03/array2hash_sorted.php
@@ -6,11 +6,19 @@ function array2hash($array){
     foreach ($array as $a){
         $new[$a[0]] = $a[1];
     }
-    // faire le tri inverse en partant de la fin si meme lettre alors l'age le + bas en premier
-    return $new;
+    $sorted = [];
+    while ($age = current($new)){
+        if ($sorted == [])
+            $sorted[] = array(key($new) => $age);
+        for ($i = 0; $i < count($sorted); $i++){
+            if ord(key($sorted[i])) 
+            $sorted[i]
+        }
+       next($new);
+    }
+    //faire le tri inverse en partant de la fin si meme lettre alors l'age le + bas en premier
+    return $sorted;
 }
 
 $array = array(array("Pierre","30"), array("Mary","28"), array("Nelly", "22"));
-
-print_r(array2hash($array));
-
+array2hash($array);
