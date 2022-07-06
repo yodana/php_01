aff656d5f3f15f45a39b85a7d2e0932334688eea
Yohann Dana <yodana@c1r3p2.42quebec.com>
2022-07-06 15:18:29 -0400

    ex06 loading

--- ex06/mendeleiev.php
+++ ex06/mendeleiev.php
@@ -1,5 +1,22 @@
 <?php
     $file = fopen("ex06.txt", "r");
+    $m = fopen("mendeleiev.html", "w");
+    // 18 colonnes 7 lignes
+    fputs($m, '<!DOCTYPE html>
+    <html>
+        <head>
+            <link rel="stylesheet" href="style.css" type="text/css" />
+            <meta charset="utf-8">
+            <title>COPY</title>
+        </head>
+        <body>');
+    
     while($line = fgets($file)){
-        echo $line . PHP_EOL;
+        print_r(explode("=", $line));    
     }
+    print_r($tab);
+    fputs($m, '</body>
+    </html>');
+
+    fclose($file);
+    fclose($m);
